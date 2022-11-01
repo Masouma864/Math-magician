e1d74f61f3857f157f7c752a04e22c390e099ad0
masouma <masouma2020@gmail.com>
2022-11-01 12:58:34 +0430

    Create Calculator.js file

---
+++ src/components/calculator.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+import './calculator.css';
+class Calculator extends Component {
+
+    render() {
+        return (
+            <div className='container'>
+                <div className="screen">0</div>
+                <div className="btn">Ac</div>
+                <div className="btn">+/-</div>
+                <div className="btn">%</div>
+                <div className="btn operator">&#xf7;</div>
+                <div className="btn">7</div>
+                <div className="btn">8</div>
+                <div className="btn">9</div>
+                <div className="btn operator">x</div>
+                <div className="btn">4</div>
+                <div className="btn">5</div>
+                <div className="btn">6</div>
+                <div className="btn operator">-</div>
+                <div className="btn">1</div>
+                <div className="btn">2</div>
+                <div className="btn">3</div>
+                <div className="btn operator">+</div>
+                <div className="btn span-2">0</div>
+                <div className="btn">.</div>
+                <div className="btn operator">=</div>
+            </div>
+        );
+    }
+}
+
+export default Calculator;
