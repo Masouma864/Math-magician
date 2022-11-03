dd6203aaded153411cea059d15eb20fbad5993d0
masouma <masouma2020@gmail.com>
2022-11-03 09:50:53 +0430

    Add event to buttons

--- src/components/calculator.js
+++ src/components/calculator.js
@@ -1,39 +1,49 @@
 import React, { Component } from 'react';
 import './calculator.css';
+import operate from '../logic/operate';
 import calculate from '../logic/calculate';
 
 class Calculator extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
+    constructor(props) {
+        super(props);
+        this.state = {
+            total: null,
+            next: null,
+            operation: null,
+        };
+        this.operationHandeler = this.operationHandeler.bind(this);
+    }
+    operationHandeler = (e) => {
+        this.setState((state) => calculate(state, e.target.textContent));
+    }
 
-  render() {
-    return (
-      <div className="container">
-        <div className="screen">0</div>
-        <div className="btn">Ac</div>
-        <div className="btn">+/-</div>
-        <div className="btn">%</div>
-        <div className="btn operator">&#xf7;</div>
-        <div className="btn">7</div>
-        <div className="btn">8</div>
-        <div className="btn">9</div>
-        <div className="btn operator">x</div>
-        <div className="btn">4</div>
-        <div className="btn">5</div>
-        <div className="btn">6</div>
-        <div className="btn operator">-</div>
-        <div className="btn">1</div>
-        <div className="btn">2</div>
-        <div className="btn">3</div>
-        <div className="btn operator">+</div>
-        <div className="btn span-2">0</div>
-        <div className="btn">.</div>
-        <div className="btn operator">=</div>
-      </div>
-    );
-  }
+    render() {
+        const { total, next, operation } = this.state;
+        return (
+            <div className="container">
+                <div onClick={this.operationHandeler} className="screen"> {total || operation || next} </div>
+                <div onClick={this.operationHandeler} className="btn">Ac</div>
+                <div onClick={this.operationHandeler} className="btn">+/-</div>
+                <div onClick={this.operationHandeler} className="btn">%</div>
+                <div onClick={this.operationHandeler} className="btn operator">&#xf7;</div>
+                <div onClick={this.operationHandeler} className="btn">7</div>
+                <div onClick={this.operationHandeler} className="btn">8</div>
+                <div onClick={this.operationHandeler} className="btn">9</div>
+                <div onClick={this.operationHandeler} className="btn operator">x</div>
+                <div onClick={this.operationHandeler} className="btn">4</div>
+                <div onClick={this.operationHandeler} className="btn">5</div>
+                <div onClick={this.operationHandeler} className="btn">6</div>
+                <div onClick={this.operationHandeler} className="btn operator">-</div>
+                <div onClick={this.operationHandeler} className="btn">1</div>
+                <div onClick={this.operationHandeler} className="btn">2</div>
+                <div onClick={this.operationHandeler} className="btn">3</div>
+                <div onClick={this.operationHandeler} className="btn operator">+</div>
+                <div onClick={this.operationHandeler} className="btn span-2">0</div>
+                <div onClick={this.operationHandeler} className="btn">.</div>
+                <div onClick={this.operationHandeler} className="btn operator">=</div>
+            </div>
+        );
+    }
 }
 
 export default Calculator;
