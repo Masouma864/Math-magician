bc83650049df6f470d2be5ca31b5e0601e8e0a36
masouma <masouma2020@gmail.com>
2022-11-04 09:16:09 +0430

    refactor to functionla component with hook

--- src/components/calculator.js
+++ src/components/calculator.js
@@ -1,53 +1,49 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './calculator.css';
 import calculate from '../logic/calculate';
 
-class Calculator extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      total: 0,
-      next: null,
-      operation: null,
-    };
-    this.operationHandeler = this.operationHandeler.bind(this);
-  }
+function Calculator() {
+    const [obj, setObj] = useState({
+        total: 0,
+        next: null,
+        operation: null,
+    });
 
-    operationHandeler = (e) => {
-      this.setState((state) => calculate(state, e.target.textContent));
-    }
-
-    render() {
-      const { total, next, operation } = this.state;
-      return (
-        <div className="container">
-          <button type="button" onClick={this.operationHandeler} className="screen">
-            {total || ''}
-            {operation || ''}
-            {next || ''}
-          </button>
-          <button type="button" onClick={this.operationHandeler} className="btn">AC</button>
-          <button type="button" onClick={this.operationHandeler} className="btn">+/-</button>
-          <button type="button" onClick={this.operationHandeler} className="btn">%</button>
-          <button type="button" onClick={this.operationHandeler} className="btn operator">&#xf7;</button>
-          <button type="button" onClick={this.operationHandeler} className="btn">7</button>
-          <button type="button" onClick={this.operationHandeler} className="btn">8</button>
-          <button type="button" onClick={this.operationHandeler} className="btn">9</button>
-          <button type="button" onClick={this.operationHandeler} className="btn operator">x</button>
-          <button type="button" onClick={this.operationHandeler} className="btn">4</button>
-          <button type="button" onClick={this.operationHandeler} className="btn">5</button>
-          <button type="button" onClick={this.operationHandeler} className="btn">6</button>
-          <button type="button" onClick={this.operationHandeler} className="btn operator">-</button>
-          <button type="button" onClick={this.operationHandeler} className="btn">1</button>
-          <button type="button" onClick={this.operationHandeler} className="btn">2</button>
-          <button type="button" onClick={this.operationHandeler} className="btn">3</button>
-          <button type="button" onClick={this.operationHandeler} className="btn operator">+</button>
-          <button type="button" onClick={this.operationHandeler} className="btn span-2">0</button>
-          <button type="button" onClick={this.operationHandeler} className="btn">.</button>
-          <button type="button" onClick={this.operationHandeler} className="btn operator">=</button>
-        </div>
-      );
-    }
 }
+
+const operationHandeler = (e) => {
+    const updatedObject = calculate(obj, e.target.textContent);
+    setObj(updatedObject);
+};
+
+return (
+    <div className="container">
+        <button type="button" onClick={this.operationHandeler} className="screen">
+            {obj.total || ''}
+            {obj.operation || ''}
+            {obj.next || ''}
+        </button>
+        <button type="button" onClick={operationHandeler} className="btn">AC</button>
+        <button type="button" onClick={operationHandeler} className="btn">+/-</button>
+        <button type="button" onClick={operationHandeler} className="btn">%</button>
+        <button type="button" onClick={operationHandeler} className="btn operator">&#xf7;</button>
+        <button type="button" onClick={operationHandeler} className="btn">7</button>
+        <button type="button" onClick={operationHandeler} className="btn">8</button>
+        <button type="button" onClick={operationHandeler} className="btn">9</button>
+        <button type="button" onClick={operationHandeler} className="btn operator">x</button>
+        <button type="button" onClick={operationHandeler} className="btn">4</button>
+        <button type="button" onClick={operationHandeler} className="btn">5</button>
+        <button type="button" onClick={operationHandeler} className="btn">6</button>
+        <button type="button" onClick={operationHandeler} className="btn operator">-</button>
+        <button type="button" onClick={operationHandeler} className="btn">1</button>
+        <button type="button" onClick={operationHandeler} className="btn">2</button>
+        <button type="button" onClick={operationHandeler} className="btn">3</button>
+        <button type="button" onClick={operationHandeler} className="btn operator">+</button>
+        <button type="button" onClick={operationHandeler} className="btn span-2">0</button>
+        <button type="button" onClick={operationHandeler} className="btn">.</button>
+        <button type="button" onClick={operationHandeler} className="btn operator">=</button>
+    </div>
+);
+
 
 export default Calculator;
