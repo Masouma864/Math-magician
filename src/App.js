90e87d7d9b4f9c4e7a7fba23d5106d867ece90aa
masouma <masouma2020@gmail.com>
2022-11-10 08:13:17 +0430

    modify app.js

--- src/App.js
+++ src/App.js
@@ -1,19 +1,24 @@
 import React from 'react';
+import {
+  Routes,
+  Route,
+} from 'react-router-dom';
+import Home from './components/Home';
+import Quote from './components/Quote';
+import Navbar from './components/Navbar';
 import Calculator from './components/calculator';
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
-
-  render() {
-    return (
-      <div className="App">
-        <Calculator />
-      </div>
-    );
-  }
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/quote" element={<Quote />} />
+        <Route path="/calculator" element={<Calculator />} />
+        <Route path="/home" element={<Home />} />
+      </Routes>
+    </>
+  );
 }
 
 export default App;
